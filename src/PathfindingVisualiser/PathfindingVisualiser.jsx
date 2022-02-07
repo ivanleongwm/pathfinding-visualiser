8e4c3c3d86b653c959b6b3be387ea5260aec2961
ivanleongwm <ivanleongwm@gmail.com>
2022-02-07 14:35:16 +0800

    PathfindingVisualiser component created

---
+++ src/PathfindingVisualiser/PathfindingVisualiser.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function PathfindingVisualiser() {
+
+    return (
+        <div>Pathfinding Visualiser Placeholder 2</div>
+    )
+}
+
+export default PathfindingVisualiser
