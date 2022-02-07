b791b3ee9a6ef1ea9a3651283a445643462be0e6
ivanleongwm <ivanleongwm@gmail.com>
2022-02-07 14:45:33 +0800

    Node component created

---
+++ src/PathfindingVisualiser/Node/Node.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Node.css'
+
+function Node() {
+    
+    return (
+        <div className="node">Node</div>
+    )
+}
+
+export default Node
--- src/PathfindingVisualiser/PathfindingVisualiser.jsx
+++ src/PathfindingVisualiser/PathfindingVisualiser.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Node from './Node/Node'
+import './PathfindingVisualiser.css'
 
 function PathfindingVisualiser() {
 
     return (
-        <div>Pathfinding Visualiser Placeholder 2</div>
+        <div>
+            <p>Pathfinding Visualiser Placeholder 2</p>
+            <Node/>
+        </div>
     )
 }
 
